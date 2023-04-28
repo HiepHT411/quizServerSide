350266654131366538cdb18b164338e53d3890e1
gio-del <giovanni.delucia@polimi.it>
2023-04-28 21:23:47 +0200

    first commit

---
+++ src/main/java/com/example/GoQuiz/GoQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GoQuiz/GoQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GoQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
